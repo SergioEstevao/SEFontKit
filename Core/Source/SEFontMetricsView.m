819988ca8738417135285f97862ba116d66239f9
Sergio Estevao <sergio.estevao@tigerspike.com>
2013-05-19 21:33:25 +0100

    Change default colour for view metrics.

--- Core/Source/SEFontMetricsView.m
+++ Core/Source/SEFontMetricsView.m
@@ -14,12 +14,7 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
-        self.ascenderColor = [UIColor redColor];
-        self.descenderColor = [UIColor greenColor];
-        self.baseLineColor = [UIColor lightGrayColor];
-        self.capColor = [UIColor purpleColor];
-        self.xColor = [UIColor yellowColor];
+        [self setupColors];
     }
     return self;
 }
@@ -27,13 +22,18 @@
 - (id)initWithCoder:(NSCoder *)decoder {
     self = [super initWithCoder:decoder];
     if (self){
-        self.ascenderColor = [UIColor colorWithRed:1 green:0 blue:0 alpha:1];
-        self.descenderColor = [UIColor colorWithRed:0 green:1 blue:0 alpha:1];
-        self.baseLineColor = [UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:1];
-        self.capColor = [UIColor purpleColor];
-        self.xColor = [UIColor yellowColor];
+        [self setupColors];
     }
     return self;
+}
+
+- (void) setupColors {
+    // Initialization code
+    self.ascenderColor = [UIColor redColor];
+    self.descenderColor = [UIColor greenColor];
+    self.baseLineColor = [UIColor lightGrayColor];
+    self.capColor = [UIColor purpleColor];
+    self.xColor = [UIColor blueColor];
 }
 
 // Only override drawRect: if you perform custom drawing.
@@ -44,12 +44,12 @@
     // Drawing code
     CGContextRef context = UIGraphicsGetCurrentContext ();
     
-    CGFloat baseLine = self.font.lineHeight + self.font.descender -1.0f ;
+    CGFloat baseLine = self.font.lineHeight + self.font.descender -1.0f;
     CGFloat ascender = ceilf(baseLine -  self.font.ascender);
-    CGFloat descender = baseLine - self.font.descender;
-    CGFloat capHeight = baseLine -  self.font.capHeight;
-    CGFloat xHeight = baseLine - self.font.xHeight;
-
+    CGFloat descender = ceilf(baseLine - self.font.descender);
+    CGFloat capHeight = ceilf(baseLine -  self.font.capHeight);
+    CGFloat xHeight = ceilf(baseLine - self.font.xHeight);
+    baseLine = ceilf(baseLine);
     //baseLine
     CGContextMoveToPoint(context, 0, baseLine);
     CGContextAddLineToPoint(context, self.frame.size.width, baseLine);
